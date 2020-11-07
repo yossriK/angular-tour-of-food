d48c2351f9cbcfbc7b7ffe1bd70a0c72a14a69fe
Yossri Khalil <yossrikhalil@nureva.com>
2020-11-07 13:24:48 -0700

    testing branching

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
+// ading a acomment to show rashik how this wokrs
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
